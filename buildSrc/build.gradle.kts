f9b99fd44fe4541000fa3582c4a88bb211b506b0
Leandro Santana <leo93santana@gmail.com>
2025-04-21 16:58:52 -0300

    🔨 buildSrc scripts

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    //kotlin("multiplatform") version "2.1.10"
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+}
+
+dependencies {
+}
---
+++ buildSrc/src/main/kotlin/ConfigsConstants.kt
@@ -0,0 +1,11 @@
+object ConfigsConstants {
+    const val group = "io.github.sanlean"
+    const val version = "0.0.1"
+    const val namespace = "io.github.sanlean.minesweeper"
+    const val compileSdk = 34
+    const val minSdk = 30
+    const val project = "minesweeper-core"
+    const val projectName = "Minesweeper Core"
+    const val projectDescription = "Kotlin Multiplatform library for Minesweeper game engine."
+    const val scmUrl = "https://github.com/sanlean/minesweeper"
+}
